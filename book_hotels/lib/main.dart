cefd860b456cab2fe591bd848fe57f8a9329f0ab
Sagnik Bhattacharya <sagnikbhattacharya1506@gmail.com>
2020-12-02 21:36:21 +0530

    ..

--- book_hotels/lib/main.dart
+++ book_hotels/lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
